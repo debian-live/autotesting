baddf6ec6f7110d98b460342e8d5fcd21756f992
Brendan M. Sleight <bms.debian@barwap.com>
2008-09-01 20:36:56 +0000

    Test lenny_live_beta1/i386/usb-hdd/ -  lenny_live_beta.lst
    Signed-off-by: Brendan M. Sleight <bms.debian@barwap.com>

--- clean-old-files.sh
+++ clean-old-files.sh
@@ -5,6 +5,7 @@ find /home/autotesting/*/daily/ -not -type d -ctime +4 -print0     | xargs --nul
 find /var/www/debian-live/i386/*-sid-*  -not -type d -ctime +4 -print0   | xargs --null --no-run-if-empty rm -f
 find /var/www/webconverger/i386/*  -not -type d -ctime +4 -print0   | xargs --null --no-run-if-empty rm -f
 find /var/www/local-build/debian-live/i386/*  -not -type d -ctime +4 -print0   | xargs --null --no-run-if-empty rm -f
+find /var/www/lenny_live_beta1/i386/*  -not -type d -ctime +4 -print0   | xargs --null --no-run-if-empty rm -f
 
 find /home/autotesting/*/weekly/ -not -type d -ctime +28 -print0   | xargs --null --no-run-if-empty rm -f
 find /var/www/debian-live/i386/*-lenny-*  -not -type d -ctime +28 -print0   | xargs --null --no-run-if-empty rm -f
